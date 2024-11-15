c6fa7ba458c1d1147b4676319889e2a54884de55
Kumar Biswajit Ojha <kumar.biswajit@rapidsoft.co.in>
2024-11-15 18:34:33 +0530

    @customElements added

--- src/features/login/Login.jsx
+++ src/features/login/Login.jsx
@@ -1,7 +1,32 @@
+import { Box } from "@mui/material";
 import React from "react";
+import {
+  CustomButton,
+  CustomTextField,
+  CustomAutocomplete,
+  CustomAutocompleteMultiSelect,
+  CustomDatePicker,
+} from "../../components/CustomElements";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <Box
+      sx={{
+        padding: 2,
+        background: "#ccc",
+      }}
+    >
+      <CustomTextField label={"Description"} isRequired={true} />
+      <CustomButton color="secondary">Add recommendation</CustomButton>
+      <CustomAutocomplete options={["ok", "YO gouys"]} label="Nameee" />
+      <CustomAutocompleteMultiSelect
+        options={["ok", "YO gouys"]}
+        label="Nameee"
+        labelRenderer={(option) => option}
+      />
+      <CustomDatePicker label={"Select Date"} />
+    </Box>
+  );
 };
 
 export default Login;
