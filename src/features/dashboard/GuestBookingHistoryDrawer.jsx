9b34b87837453896594a34886ef99bf6b1b7dec8
Nikhil Khuntia <nikhil.khuntia@rapidsoft.co.in>
2024-11-25 09:52:37 +0530

    @advance payment validation

--- src/features/dashboard/GuestBookingHistoryDrawer.jsx
+++ src/features/dashboard/GuestBookingHistoryDrawer.jsx
@@ -352,6 +352,7 @@ const CancelRoomDialog = ({ open, onClose, selectedBookingRefNumber }) => {
             variant="contained"
             onClick={handleSubmit}
             sx={{ backgroundColor: "#228B22" }}
+            disabled={Boolean(rejectionReason) ? "" : true}
           >
             Yes
           </Button>
