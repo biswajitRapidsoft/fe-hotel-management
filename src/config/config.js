0eb10e1d9ab7090457a3374f70b8cca6be2dc960
somyaRapidsoft <somya.ranjan@rapidsoft.co.in>
2024-12-02 18:50:41 +0530

    FrontDesk Dashboard Functionalities Wrapup

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 const config = Object.freeze({
   // baseUrl: "http://192.168.12.41:9000/",
-  baseUrl: "http://192.168.30.44:9000/",
+  baseUrl: "http://192.168.12.37:9000/",
+  // baseUrl: "http://192.168.30.44:9000/",
   apiName: {
     loginAsCustomer: "login/as/customer",
     loginAsStaff: "login",
