194ce2b3abbc9df34e164031a04514b50428be6e
Kumar Biswajit Ojha <kumar.biswajit@rapidsoft.co.in>
2025-03-25 12:34:26 +0530

    @urlStaging

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ const config = Object.freeze({
   // baseUrl: "http://192.168.12.37:9000/",
   // baseUrl: "http://192.168.12.43:9000/",
   // baseUrl: "http://192.168.30.44:8070/be-hms/",
-  baseUrl: "http://crimarrstaging.nyggs.com/backend/",
+  // baseUrl: "http://crimarrstaging.nyggs.com/backend/",
   // baseUrl: "http://192.168.30.44:9000/",
   // baseUrl: "http://192.168.12.43:8080/be-hms/",
-  // baseUrl: "https://hms.watsoo.com/backend/",
+  baseUrl: "https://hms.watsoo.com/backend/",
   // baseUrl: "http://192.168.30.44:8070/be-hms/", // HO Staging
   // baseUrl: "http://192.168.12.3:9000/",
   // baseUrl: "http://192.168.30.44:8070/be-hms/",
